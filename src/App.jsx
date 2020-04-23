91a8f2237ab34416fb66b117c888d282bdd30fe1
yamiquispe <yamipqm@gmail.com>
2020-04-22 23:02:19 -0500

    Estilos de juego de cartas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import './css/styles.css';
 import Header from './components/Header';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import ImgTerramar from '../img/cuentos-de-terramar.jpg';
+import ImgCastillo from '../img/el-castillo-ambulante.jpg';
+import ImgChijiro from '../img/el-viaje-de-chijiro.jpg';
+import ImgMononoke from '../img/la-princesa-mononoke.jpg';
+import ImgLuciernagas from '../img/la-tumba-de-las-luciernagas.jpg';
+import ImgTotoro from '../img/mi-vecino-totoro.jpg';
+import ImgYamada from '../img/mis-vecinos-los-yamada.jpg';
+import ImgSusurros from '../img/susurros-del-corazon.jpg';
+
+
+const Cards = () => (
+  <main className="Game">
+    <section className="Cards">
+      <article>
+        <img src={ImgTerramar} alt="Cuentos de Terramar" />
+      </article>
+      <article>
+        <img src={ImgCastillo} alt="El castillo ambulante" />
+      </article>
+      <article>
+        <img src={ImgChijiro} alt="El viaje de Chijiro" />
+      </article>
+      <article>
+        <img src={ImgMononoke} alt="La princesa Mononoke" />
+      </article>
+      <article>
+        <img src={ImgLuciernagas} alt="La tumba de las luciérnagas" />
+      </article>
+      <article>
+        <img src={ImgTotoro} alt="Mi vecino Totoro" />
+      </article>
+      <article>
+        <img src={ImgYamada} alt="Mis vecinos Los Yamada" />
+      </article>
+      <article>
+        <img src={ImgSusurros} alt="Susurros del corazón" />
+      </article>
+      <article>
+        <img src={ImgTerramar} alt="Cuentos de Terramar" />
+      </article>
+      <article>
+        <img src={ImgCastillo} alt="El castillo ambulante" />
+      </article>
+      <article>
+        <img src={ImgChijiro} alt="El viaje de Chijiro" />
+      </article>
+      <article>
+        <img src={ImgMononoke} alt="La princesa Mononoke" />
+      </article>
+      <article>
+        <img src={ImgLuciernagas} alt="La tumba de las luciérnagas" />
+      </article>
+      <article>
+        <img src={ImgTotoro} alt="Mi vecino Totoro" />
+      </article>
+      <article>
+        <img src={ImgYamada} alt="Mis vecinos Los Yamada" />
+      </article>
+      <article>
+        <img src={ImgSusurros} alt="Susurros del corazón" />
+      </article>
+    </section>
+  </main>
+);
+
+
+export default Cards;
