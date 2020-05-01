a3dcb0865f6ec9cd305b87c6b9d653eb6cff6f4d
yamiquispe <yamipqm@gmail.com>
2020-04-30 22:14:28 -0500

    Match de cartas con efecto flip

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -2,19 +2,62 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ItemCard from './ItemCard';
-import cards from '../data/cards';
+import ImgTerramar from '../img/cuentos-de-terramar.jpg';
+import ImgCastillo from '../img/el-castillo-ambulante.jpg';
+import ImgChijiro from '../img/el-viaje-de-chijiro.jpg';
+import ImgMononoke from '../img/la-princesa-mononoke.jpg';
+import ImgLuciernagas from '../img/la-tumba-de-las-luciernagas.jpg';
+import ImgTotoro from '../img/mi-vecino-totoro.jpg';
+import ImgYamada from '../img/mis-vecinos-los-yamada.jpg';
+import ImgSusurros from '../img/susurros-del-corazon.jpg';
+
+
+const dataCards = [
+  { url: ImgTerramar, id: 'img01', alt: 'Cuentos de Terramar' },
+  { url: ImgCastillo, id: 'img02', alt: 'El castillo ambulante' },
+  { url: ImgChijiro, id: 'img03', alt: 'El viaje de Chijiro' },
+  { url: ImgMononoke, id: 'img04', alt: 'La princesa Mononoke' },
+  { url: ImgLuciernagas, id: 'img05', alt: 'La tumba de las luciérnagas' },
+  { url: ImgTotoro, id: 'img06', alt: 'Mi vecino Totoro' },
+  { url: ImgYamada, id: 'img07', alt: 'Mis vecinos Los Yamada' },
+  { url: ImgSusurros, id: 'img08', alt: 'Susurros del corazón' },
+  { url: ImgTerramar, id: 'img09', alt: 'Cuentos de Terramar' },
+  { url: ImgCastillo, id: 'img10', alt: 'El castillo ambulante' },
+  { url: ImgChijiro, id: 'img11', alt: 'El viaje de Chijiro' },
+  { url: ImgMononoke, id: 'img12', alt: 'La princesa Mononoke' },
+  { url: ImgLuciernagas, id: 'img13', alt: 'La tumba de las luciérnagas' },
+  { url: ImgTotoro, id: 'img14', alt: 'Mi vecino Totoro' },
+  { url: ImgYamada, id: 'img15', alt: 'Mis vecinos Los Yamada' },
+  { url: ImgSusurros, id: 'img16', alt: 'Susurros del corazón' },
+];
 
 
 const GameCards = () => {
+  let arrayCard = [];
+
   const flipCard = (id) => {
     console.log(id);
     const element = document.querySelector(`.${id}`);
-
     element.classList.add('flip');
 
-    console.log(element);
+    arrayCard.push(element);
 
-    console.log(element.dataset.card);
+    if (arrayCard.length === 2) {
+      if (arrayCard[0].dataset.card === arrayCard[1].dataset.card) {
+        console.log('Cartas pares');
+        arrayCard[0].removeEventListener('click', flipCard);
+        arrayCard[1].removeEventListener('click', flipCard);
+        arrayCard = [];
+      } else {
+        console.log('Cartas impares');
+        setTimeout(() => {
+          console.log(arrayCard[0], 'acá ggggg');
+          arrayCard[0].classList.remove('flip');
+          arrayCard[1].classList.remove('flip');
+          arrayCard = [];
+        }, 1500);
+      }
+    }
   };
 
   return (
@@ -22,7 +65,7 @@ const GameCards = () => {
       <Header hide={false} />
       <main className="Game">
         <section className="Cards">
-          {cards.sort(() => (0.5 - Math.random()))
+          {dataCards.sort(() => (0.5 - Math.random()))
             .map((card) => <ItemCard img={card} key={card.id} flipCard={flipCard} />)}
         </section>
       </main>
