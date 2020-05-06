6622e3463aa9a17685f7fc7bf28124a98474d43b
yamiquispe <yamipqm@gmail.com>
2020-05-06 03:51:44 -0500

    Efecto de invisibilización de cartas pares

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ItemCard from './ItemCard';
@@ -6,28 +6,39 @@ import dataCards from '../data/cards';
 
 
 const GameCards = () => {
-  let arrayCard = [];
+/*   const [arrayCards, setArrayCards] = useState(dataCards); */
+  const [pairOfCards, setPairCards] = useState([]);
 
   const flipCard = (id) => {
-    console.log(id);
     const element = document.querySelector(`.${id}`);
     element.classList.add('flip');
 
-    arrayCard.push(element);
+    pairOfCards.push(element);
 
-    if (arrayCard.length === 2) {
-      if (arrayCard[0].dataset.card === arrayCard[1].dataset.card) {
+    setPairCards(pairOfCards);
+
+    console.log(pairOfCards);
+
+    if (pairOfCards.length === 2) {
+      if (pairOfCards[0].dataset.card === pairOfCards[1].dataset.card) {
         console.log('Cartas pares');
-        arrayCard[0].removeEventListener('click', flipCard);
-        arrayCard[1].removeEventListener('click', flipCard);
-        arrayCard = [];
+
+        pairOfCards[0].removeEventListener('click', flipCard);
+        pairOfCards[1].removeEventListener('click', flipCard);
+
+        setTimeout(() => {
+          pairOfCards[0].classList.add('hidden');
+          pairOfCards[1].classList.add('hidden');
+        }, 1500);
+
+        setPairCards([]);
       } else {
         console.log('Cartas impares');
+
         setTimeout(() => {
-          console.log(arrayCard[0], 'acá ggggg');
-          arrayCard[0].classList.remove('flip');
-          arrayCard[1].classList.remove('flip');
-          arrayCard = [];
+          pairOfCards[0].classList.remove('flip');
+          pairOfCards[1].classList.remove('flip');
+          setPairCards([]);
         }, 1500);
       }
     }
@@ -38,7 +49,7 @@ const GameCards = () => {
       <Header hide={false} />
       <main className="Game">
         <section className="Cards">
-          {dataCards.sort(() => (0.5 - Math.random()))
+          {dataCards/* .sort(() => (0.5 - Math.random())) */
             .map((card) => <ItemCard img={card} key={card.id} flipCard={flipCard} />)}
         </section>
       </main>
