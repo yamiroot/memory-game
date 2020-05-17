9f5fd2be80f54c982735b8a209b84e21071d5763
yamiquispe <yamipqm@gmail.com>
2020-05-16 19:12:23 -0500

    Avance en test del componente Game Cards

--- src/test/GameCards.test.jsx
+++ src/test/GameCards.test.jsx
@@ -2,67 +2,132 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import {
-  render, screen, cleanup, fireEvent,
+  render, screen, cleanup, fireEvent, act,
 } from '@testing-library/react';
-import HashRouter from '../Router';
+import { waitFor } from '@testing-library/dom';
 import dataCards from '../data/cards';
 import GameCards from '../components/GameCards';
 
 
 afterEach(cleanup);
 
-/*
-const renderWithRouter = (component) => {
+
+it('Se renderiza el componente Game Cards con 16 cartas.', () => {
   const history = createMemoryHistory();
-  return {
-    ...render(
-      <Router history={history}>
-        {component}
-      </Router>,
-    ),
-  };
-};
+  history.push('/gamecards');
 
-
-it('Se renderiza como vista principal a Game Cards.', () => {
-  const { container, getByTestId } = renderWithRouter(<HashRouter />);
-  const main = getByTestId('game');
-
-  // La página renderiza el componente GameCards
-  expect(container).toContainElement(main);
-});
- */
-
-
-it('Se renderiza la vista Game Cards al dar click en el botón "game-button".', () => {
-  const history = createMemoryHistory();
-  history.push('/gamerules');
-
-  const { container, getByTestId } = render(<Router history={history}><HashRouter /></Router>);
-
-  expect(container).toContainElement(getByTestId('rules'));
-
-  fireEvent.click(getByTestId('game-link'));
-
-  // La página renderiza el componente Header que tiene la clase "Header" cuyo texto
-  // del botón es "Reglas del juego"
-  expect(getByTestId('header-game')).toHaveClass('Header');
-  expect(screen.getByText('Reglas del juego')).toBeTruthy();
-  // La página renderiza el componente GameCards
-  expect(container).toContainElement(getByTestId('game'));
-  // La página renderiza el componente Footer
-  expect(screen.getByRole('contentinfo')).not.toBeEmpty();
-});
-
-
-it('Se renderiza la vista Game Cards con cada una de sus cartas.', () => {
-  const history = createMemoryHistory();
-  const { container, getByRole, getAllByRole } = render(
+  const container = render(
     <Router history={history}>
       <GameCards dataCards={dataCards} />
     </Router>,
   );
 
-  expect(container).toContainElement(getByRole('application'));
-  expect(getAllByRole('presentation')).toBeTruthy();
+  const nodeList = container.getAllByRole('presentation');
+
+  // Renderiza el componente Game Cards
+  expect(container.getByTestId('view-game')).toBeInTheDocument();
+
+  // Renderiza el componente Header que tiene la clase "Header" cuyo texto
+  // del botón es "Reglas del juego"
+  expect(container.getByTestId('header-game')).toHaveClass('Header');
+  expect(screen.getByText('Reglas del juego')).toBeTruthy();
+  // Renderiza las 16 cartas
+  expect(nodeList.length).toBe(16);
+  expect(nodeList[2]).toHaveAttribute('class', 'img03');
+  // Renderiza el componente Footer
+  expect(screen.getByRole('contentinfo')).not.toBeEmpty();
 });
+
+
+it('Al dar click a un card se le añade la clase "flip", con ello ocurre el efecto flip.', () => {
+  const history = createMemoryHistory();
+  const { container, getByTestId } = render(
+    <Router history={history}>
+      <GameCards name="Juego de cartas" dataCards={dataCards} />
+    </Router>,
+  );
+
+  // El componente Game Cards contiene al card de class "img01"
+  expect(container).toContainElement(getByTestId('img01'));
+  expect(getByTestId('img01')).toHaveClass('img01');
+
+  // Damos click al card
+  act(() => {
+    fireEvent.click(getByTestId('img01'));
+  });
+
+  // Se le añade la clase "flip"
+  expect(getByTestId('img01')).toHaveAttribute('class', 'img01 flip');
+});
+
+
+it('Al dar click a dos cards pares se les añade la clase "hide", con ello desaparecen.', (async () => {
+  const history = createMemoryHistory();
+  const { container, getByTestId } = render(
+    <Router history={history}>
+      <GameCards name="Juego de cartas" dataCards={dataCards} />
+    </Router>,
+  );
+
+  // El componente Game Cards contiene a los cards de class "img01" y "img09"
+  expect(container).toContainElement(getByTestId('img01'));
+  expect(container).toContainElement(getByTestId('img09'));
+
+  // Damos click al card "img01"
+  await act(async () => {
+    fireEvent.click(getByTestId('img01'));
+  });
+
+  // Se le añade la clase "flip"
+  expect(getByTestId('img01')).toHaveAttribute('class', 'img01 flip');
+
+  // Damos click al card "img09"
+  await act(async () => {
+    fireEvent.click(getByTestId('img09'));
+  });
+
+  // Se le añade la clase "flip"
+  expect(getByTestId('img09')).toHaveAttribute('class', 'img09 flip');
+
+  // Después de 1.5 segundos ambos cards pares desaparecen
+  await waitFor(() => {
+    expect(getByTestId('img01')).toHaveAttribute('class', 'img01 flip hidden');
+    expect(getByTestId('img09')).toHaveAttribute('class', 'img09 flip hidden');
+  });
+}));
+
+
+it('Al dar click a dos cards impares se les añade la clase "flip", y luego se remueve esa misma clase.', (async () => {
+  const history = createMemoryHistory();
+  const { container, getByTestId } = render(
+    <Router history={history}>
+      <GameCards name="Juego de cartas" dataCards={dataCards} />
+    </Router>,
+  );
+
+  // El componente Game Cards contiene a los cards de class "img01" y "img09"
+  expect(container).toContainElement(getByTestId('img01'));
+  expect(container).toContainElement(getByTestId('img07'));
+
+  // Damos click al card "img01"
+  act(() => {
+    fireEvent.click(getByTestId('img01'));
+  });
+
+  // Se le añade la clase "flip"
+  expect(getByTestId('img01')).toHaveAttribute('class', 'img01 flip');
+
+  // Damos click al card "img07"
+  act(() => {
+    fireEvent.click(getByTestId('img07'));
+  });
+
+  // Se le añade la clase "flip"
+  expect(getByTestId('img07')).toHaveAttribute('class', 'img07 flip');
+
+  // Después de 1.5 segundos se remueve el efecto flip a ambos cards
+  await waitFor(() => {
+    expect(getByTestId('img01')).toHaveAttribute('class', 'img01');
+    expect(getByTestId('img07')).toHaveAttribute('class', 'img07');
+  });
+}));
--- src/test/ItemCards.test.jsx
+++ src/test/ItemCards.test.jsx
@@ -5,8 +5,6 @@ import {
   render, cleanup, fireEvent, act,
 } from '@testing-library/react';
 import { waitFor } from '@testing-library/dom';
-import dataCards from '../data/cards';
-import GameCards from '../components/GameCards';
 import ItemCard from '../components/ItemCard';
 
 
@@ -17,19 +15,6 @@ const card = {
   url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hobbyconsolas.com%2Freviews%2Fcritica-haru-reino-gatos-pelicula-animacion-studio-ghibli-591431&psig=AOvVaw2EM1PyApyTUL8dX6igbABG&ust=1589671445839000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjqnOWBt-kCFQAAAAAdAAAAABAD',
   id: 'img01',
   alt: 'Haru en el reino de los gatos',
-};
-
-
-const renderWithRouter = (newimg, flipCard) => {
-  const history = createMemoryHistory();
-
-  return {
-    ...render(
-      <Router history={history}>
-        <ItemCard img={newimg} flipCard={() => flipCard(newimg.id)} />
-      </Router>,
-    ),
-  };
 };
 
 
@@ -45,12 +30,11 @@ it('Se renderiza una carta del juego.', (async () => {
   expect(container).toContainElement(getByTestId('img01'));
   expect(getByTestId('img01')).toHaveClass('img01');
 
-  console.log(getByTestId('img01'));
   act(() => {
     fireEvent.click(getByTestId('img01'));
   });
 
-  console.log(getByTestId('img01'));
+  expect(flipCard).toHaveBeenCalled();
 
   await waitFor(() => {
     expect(getByTestId('img01')).toHaveAttribute('class', 'img01 flip');
