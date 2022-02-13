1b78f4900e16d052df1e8cd51da6bc1e43cbebf9
yamiroot <yamipqm@gmail.com>
2022-02-13 07:54:57 -0500

    Updates

---
+++ precache-manifest.774e44b2be3d05a0750aa03470f659e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417637106956712d1614525cfa699bce",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "be77a94cea7c4f8b7a03",
+    "url": "/memory-game/static/css/main.ac59fce9.chunk.css"
+  },
+  {
+    "revision": "664bddee668a5a323c6e",
+    "url": "/memory-game/static/js/2.e962a359.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/memory-game/static/js/2.e962a359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be77a94cea7c4f8b7a03",
+    "url": "/memory-game/static/js/main.52fc9889.chunk.js"
+  },
+  {
+    "revision": "ad138a7676febb402a84",
+    "url": "/memory-game/static/js/runtime-main.c98553d9.js"
+  },
+  {
+    "revision": "f2879c73d504d3c2c4db84b4ff8b09eb",
+    "url": "/memory-game/static/media/cuentos-de-terramar.f2879c73.jpg"
+  },
+  {
+    "revision": "c97792fbe6d44ddbcedcc87e681e6b06",
+    "url": "/memory-game/static/media/el-castillo-ambulante.c97792fb.jpg"
+  },
+  {
+    "revision": "9fa7ea9d96c28b93383d934188f30770",
+    "url": "/memory-game/static/media/el-viaje-de-chijiro.9fa7ea9d.jpg"
+  },
+  {
+    "revision": "1b826e0e37bd3d74ac98cfaa069360fc",
+    "url": "/memory-game/static/media/la-princesa-mononoke.1b826e0e.jpg"
+  },
+  {
+    "revision": "e04fa3949bbe6ba065b5d303217f38ce",
+    "url": "/memory-game/static/media/la-tumba-de-las-luciernagas.e04fa394.jpg"
+  },
+  {
+    "revision": "14a4fbf16cacecc4624987cd0f2ae508",
+    "url": "/memory-game/static/media/logoGame.14a4fbf1.png"
+  },
+  {
+    "revision": "39f14cb102484d75a57908eb44cd54e9",
+    "url": "/memory-game/static/media/mi-vecino-totoro.39f14cb1.jpg"
+  },
+  {
+    "revision": "7bda1f507bbc29634389545227bb6ff1",
+    "url": "/memory-game/static/media/mis-vecinos-los-yamada.7bda1f50.jpg"
+  },
+  {
+    "revision": "f90962285038d401deb0b1ea9caf0a7a",
+    "url": "/memory-game/static/media/susurros-del-corazon.f9096228.jpg"
+  }
+]);
